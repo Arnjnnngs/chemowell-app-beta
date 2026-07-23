8f56335a028db57e8ad456313e10e480a83f4eaa
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-23 15:06:01 -0500

    app-v3: empty-start med list + interactive first-run walkthrough

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chemowell-app-v2';
+const CACHE = 'chemowell-app-v3';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
