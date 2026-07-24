6973e2a3b506e5fba6dcc1251be8cf0978994c2d
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-23 19:27:41 -0500

    app-v7: audit P0 fixes - missed tracking for user meds, reserved legacy ids, generic chemo banner, day-1 gating, license-safe erase, ghost copy purge

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chemowell-app-v6b';
+const CACHE = 'chemowell-app-v7';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
