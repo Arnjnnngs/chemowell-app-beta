1732aa02133990a37590113793c5fe5e58647495
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-23 22:58:52 -0500

    app-v9: design batch 1 (ivory canvas, solid cards, deeper rose), chemo/radiation copy, As-needed days, remove On-device chip

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chemowell-app-v8c';
+const CACHE = 'chemowell-app-v9';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
