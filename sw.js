7b2e64ad6bb5751a3b02f40032a8fd2248c6cf7a
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-27 02:42:33 -0500

    app-v20: pause a medication, excluded-near-treatment mode, schedule-window time picker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chemowell-app-v19';
+const CACHE = 'chemowell-app-v20';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
