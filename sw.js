68a2125e8d49d2929dfde0c065a11c79a4f4c7da
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-28 14:31:25 -0500

    v25: clearer daily-limit/unit copy, rounded-select confirm, schedule-window save message, mg-mismatch preview + toast wording

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chemowell-app-v24';
+const CACHE = 'chemowell-app-v25';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
