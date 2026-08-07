e82f061ab201345a6eaa767d584be5304ff9a255
Arnjnnngs <arnjnnngs@gmail.com>
2026-08-07 08:48:43 -0500

    app-v39: bump sw.js cache for double-tap fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chemowell-app-v39';
+const CACHE = 'chemowell-app-v39-1';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
